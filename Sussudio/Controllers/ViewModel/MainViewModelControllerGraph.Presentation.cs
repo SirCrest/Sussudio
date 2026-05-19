a256500877f3627a9f3c4374dd198b2fc43a54ed
crest <crest@local>
2026-05-19 13:25:56 -0700

    refactor: split presentation graph wiring

---
+++ Sussudio/Controllers/ViewModel/MainViewModelControllerGraph.Presentation.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Threading;
+using Sussudio.Controllers;
+
+namespace Sussudio.ViewModels;
+
+public partial class MainViewModel
+{
+    private sealed partial class MainViewModelControllerGraph
+    {
+        private static MainViewModelUiDispatchController CreateUiDispatchController(MainViewModel viewModel)
+        {
+            return new MainViewModelUiDispatchController(
+                new MainViewModelUiDispatchControllerContext
+                {
+                    DispatcherQueue = viewModel._dispatcherQueue,
+                    IsDisposing = () => Volatile.Read(ref viewModel._disposeState) != 0,
+                    Log = message => Logger.Log(message),
+                    LogException = exception => Logger.LogException(exception),
+                    SetStatusText = value => viewModel.StatusText = value,
+                });
+        }
+
+        private static MainViewModelPreviewLifecycleController CreatePreviewLifecycleController(MainViewModel viewModel)
+        {
+            return new MainViewModelPreviewLifecycleController(
+                new MainViewModelPreviewLifecycleControllerContext
+                {
+                    SessionCoordinator = viewModel._sessionCoordinator,
+                    BuildCaptureSettings = viewModel.BuildCaptureSettings,
+                    InvokeOnUiThreadAsync = (operation, cancellationToken) => viewModel.InvokeOnUiThreadAsync(operation, cancellationToken),
+                    RampPreviewVolumeDownForStopAsync = viewModel.RampPreviewVolumeDownForStopAsync,
+                    CreateReinitializeController = controller => new MainViewModelPreviewReinitializeController(
+                        new MainViewModelPreviewReinitializeControllerContext
+                        {
+                            SelectedDevice = () => viewModel.SelectedDevice,
+                            SelectedFormat = () => viewModel.SelectedFormat,
+                            IsRecording = () => viewModel.IsRecording,
+                            IsInitialized = () => viewModel.IsInitialized,
+                            SetIsInitialized = value => viewModel.IsInitialized = value,
+                            IsPreviewing = () => viewModel.IsPreviewing,
+                            IsPreviewReinitializing = () => viewModel.IsPreviewReinitializing,
+                            SetIsPreviewReinitializing = value => viewModel.IsPreviewReinitializing = value,
+                            SetStatusText = value => viewModel.StatusText = value,
+                            CancelPreviewRestartAfterReinitialize = () => viewModel._cancelPreviewRestartAfterReinitialize,
+                            SetCancelPreviewRestartAfterReinitialize = value => viewModel._cancelPreviewRestartAfterReinitialize = value,
+                            IncrementReinitializeGeneration = () => Interlocked.Increment(ref viewModel._previewReinitializeGeneration),
+                            ReadReinitializeGeneration = () => Volatile.Read(ref viewModel._previewReinitializeGeneration),
+                            PendingFlashbackCycleTask = () => viewModel._pendingFlashbackCycleTask,
+                            ClearPendingFlashbackCycleIfSameAndCompleted = task =>
+                            {
+                                if (ReferenceEquals(viewModel._pendingFlashbackCycleTask, task) && task.IsCompleted)
+                                {
+                                    viewModel._pendingFlashbackCycleTask = null;
+                                }
+                            },
+                            WaitReinitializeGateAsync = viewModel._previewReinitializeGate.WaitAsync,
+                            ReleaseReinitializeGate = () => viewModel._previewReinitializeGate.Release(),
+                            NotifyPreviewReinitRequestedAsync = viewModel.NotifyPreviewReinitRequestedAsync,
+                            NotifyRendererStopAsync = viewModel.NotifyRendererStopAsync,
+                        },
+                        controller),
+                    SelectedDevice = () => viewModel.SelectedDevice,
+                    SetSelectedDevice = device => viewModel.SelectedDevice = device,
+                    IsInitialized = () => viewModel.IsInitialized,
+                    SetIsInitialized = value => viewModel.IsInitialized = value,
+                    IsPreviewing = () => viewModel.IsPreviewing,
+                    SetIsPreviewing = value => viewModel.IsPreviewing = value,
+                    IsPreviewReinitializing = () => viewModel.IsPreviewReinitializing,
+                    IsRecording = () => viewModel.IsRecording,
+                    ShouldStartAudioPreview = () => viewModel.IsAudioPreviewEnabled && viewModel.IsAudioEnabled,
+                    IsAudioPreviewActive = () => viewModel._captureService.IsAudioPreviewActive,
+                    SetStatusText = value => viewModel.StatusText = value,
+                    RaisePreviewStartRequested = () => viewModel.PreviewStartRequested?.Invoke(viewModel, EventArgs.Empty),
+                    RaisePreviewStopRequested = () => viewModel.PreviewStopRequested?.Invoke(viewModel, EventArgs.Empty),
+                    ApplyLatestSourceTelemetryForPreviewStart = () =>
+                        viewModel._sourceTelemetryController.ApplySourceTelemetrySnapshot(
+                            viewModel._captureService.GetLatestSourceTelemetrySnapshot(),
+                            allowAutoRetarget: true),
+                });
+        }
+    }
+}
